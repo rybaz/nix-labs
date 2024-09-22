bd15b46161322bb376d494aa531ce9c7b454a571
rybaz <>
2024-09-22 13:49:38 -0400

    flake stuff

--- 4_devshell_flake_e2g/flake.nix
+++ 4_devshell_flake_e2g/flake.nix
@@ -5,11 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }:
   let
-  	pkgs = nixpkgs.legacyPackages."x86_64-linux";
-  in
-  	devshells."x86_64-linux".default =
-		import ./shell.nix {inherit pkgs; };
+  	pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+  in {
+  	devShells."aarch64-darwin".default =
+		pkgs.callPackage (import ./shell.nix) {};
   };
 }
